039c7b8b66de750ca5072c46f2df7c23d2dc376b
Dan Cotton <dan.cotton@capgemini.com>
2018-01-04 11:26:36 +0000

    WIP Actions on Google adaptor

---
+++ index.js
@@ -0,0 +1,8 @@
+//=========================================================
+// Import NPM modules
+//=========================================================
+const express = require("express");
+
+const router = express.Router();
+
+module.exports = router;
