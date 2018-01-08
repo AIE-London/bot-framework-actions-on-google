302cd9ed32fd178247b6d0ce69e4f7440ba1c9fd
Dan Cotton <dan.cotton@capgemini.com>
2018-01-08 10:05:29 +0000

    Moved body parser into lib

--- demo/index.js
+++ demo/index.js
@@ -2,13 +2,11 @@
 // Import NPM modules
 //=========================================================
 const express = require("express");
-const bodyParser = require("body-parser");
 const actionsOnGoogleAdapter = require("../");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(bodyParser.json());
 app.use(actionsOnGoogleAdapter(process.env.DIRECT_LINE_SECRET));
 
 app.listen(PORT, () => console.log(`ActionsOnGoogle demo listening on port ${PORT}!`));
--- index.js
+++ index.js
@@ -2,10 +2,13 @@
 // Import NPM modules
 //=========================================================
 const express = require("express");
+const bodyParser = require("body-parser");
 const fulfill = require("./lib/fulfilment");
+
 
 module.exports = (directlineSecret) => {
   const router = express.Router();
+  router.use(bodyParser.json());
   router.use(fulfill(directlineSecret));
 
   return router;
