68081409417ea75a4c8317875f587bec7e4abf01
Phil Hardwick <philipchardwick@gmail.com>
2018-01-24 13:39:01 +0000

    Add documentation

--- demo/index.js
+++ demo/index.js
@@ -2,11 +2,22 @@
 // Import NPM modules
 //=========================================================
 const express = require("express");
-const actionsOnGoogleAdapter = require("../");
+const express = require("express");
+const actionsOnGoogleAdapter = require("../")(process.env.DIRECT_LINE_SECRET);
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(actionsOnGoogleAdapter(process.env.DIRECT_LINE_SECRET));
+app.use(actionsOnGoogleAdapter.router);
+
+actionsOnGoogleAdapter.onUserSignedInHandlerProvider.registerHandler((user) => {
+  //Get additional user details from your API once account is linked and access token is available
+  // return fetch('https://your-api/user/' + user.userId, {
+  //   headers: {
+  //     Authorization: `Bearer ${user.accessToken}`
+  //   }
+  // }).then(res => res.json())
+  return Promise.resolve({username: 'John87', ...user})
+});
 
 app.listen(PORT, () => console.log(`ActionsOnGoogle demo listening on port ${PORT}!`));
